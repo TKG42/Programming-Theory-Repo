22464d3853d708e25071b93e942a4525a90d27b4
TKG42 <153872863+TKG42@users.noreply.github.com>
2025-07-05 20:52:46 -0700

    New UI additions to Title Screen
    - Added high score canvas
    - Added gameplay instruction canvas
    - Added script for OnClick methods

---
+++ Assets/Scripts/TitleUIScreenManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class TitleUIScreenManager : MonoBehaviour
+{
+    [Header("Canvas References")]
+    [SerializeField] private GameObject titleScreenCanvas;
+    [SerializeField] private GameObject howToPlayCanvas;
+    [SerializeField] private GameObject highScoreCanvas;
+
+    private void Start()
+    {
+        ShowTitleScreen(); // ensure default state
+    }
+
+    public void ShowTitleScreen()
+    {
+        titleScreenCanvas.SetActive(true);
+        howToPlayCanvas.SetActive(false);
+        highScoreCanvas.SetActive(false);
+    }
+
+    public void ShowHowToPlay()
+    {
+        titleScreenCanvas.SetActive(false);
+        howToPlayCanvas.SetActive(true);
+        highScoreCanvas.SetActive(false);
+    }
+
+    public void ShowHighScores()
+    {
+        titleScreenCanvas.SetActive(false);
+        howToPlayCanvas.SetActive(false);
+        highScoreCanvas.SetActive(true);
+    }
+}
